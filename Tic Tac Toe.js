694a17aa81408795747ac4721be5f6f753fbaf2a
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-04-17 12:18:48 +0530

    initial game setup code and reset functionality

--- Tic Tac Toe.js
+++ Tic Tac Toe.js
@@ -1,27 +1,26 @@
-// let boxes = document.querySelectorAll(".box");
-// let resetbtn = document.querySelector("#reset-btn");
-// let newGameBtn = document.querySelector("#new-btn");
-// let msgContainer = document.querySelector(".msg-container");
-// let msg = document.querySelector("#msg");
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-// let turnO = true;
+let turnO = true;
 
-// const winPatterns = [
-//     [0, 1, 2],
-//     [0, 3, 6],
-//     [0, 4, 8],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [2, 4, 6],
-//     [3, 4, 5],
-//     [6, 7, 8],
-// ];
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
 
-// const resetGame = () => {
-//     turnO = true;
-//     enableBoxes();
-//     msgContainer.classList.add("hide");
-// };
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
 
 // boxes.forEach((box) => {
 //     box.addEventListener("click", () =>{
